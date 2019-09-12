80933d2894343582ad131e7dad8290a94330198b
Steve Gordon <sgordon@hotmail.co.uk>
2019-09-12 20:42:34 +0100

    Making it a little more complex

--- StringCreateDemo/Program.cs
+++ StringCreateDemo/Program.cs
@@ -5,6 +5,7 @@ namespace StringCreateDemo
     class Program
     {
         private const char spaceSeparator = ' ';
+        private static ReadOnlySpan<char> YearPrefix => new char[] { 'I', 't', '\'', 's', ' ' };
 
         static void Main()
         {
@@ -12,17 +13,23 @@ namespace StringCreateDemo
             {
                 FirstString = "Hello",
                 SecondString = ".NET",
-                ThirdString = "friends."
+                ThirdString = "friends.",
+                ShowYear = true,
+                DateTime = new DateTime(2019, 09, 13, 09, 00, 00)
             };
-                       
-            // TODO: Concatinate the data and write to the console       
-        }
+
+            // TODO: Build the final string data and write to the console  
+                        
+            Console.ReadKey();
+        }       
 
         internal struct ContextData
         {
             public string FirstString { get; set; }
             public string SecondString { get; set; }
             public string ThirdString { get; set; }
+            public bool ShowYear { get; set; }
+            public DateTime DateTime { get; set; }
         }
     }
 }
@@ -37,13 +44,23 @@ namespace StringCreateDemo
 
 
 
+//private static int CalculateLength(in ContextData context)
+//{
+//    var length = context.FirstString.Length + 1 +
+//        context.SecondString.Length + 1 +
+//        context.ThirdString.Length;
+
+//    if (context.ShowYear)
+//    {
+//        length += 1 + YearPrefix.Length + 4;
+//    }
+
+//    return length;
+//}
 
 
 
-
-//var length = context.FirstString.Length + 1 +
-//    context.SecondString.Length + 1 +
-//    context.ThirdString.Length;
+//int length = CalculateLength(in context);
 
 //var myString = string.Create(length, context, (chars, state) =>
 //{
@@ -58,6 +75,15 @@ namespace StringCreateDemo
 //    chars[position++] = spaceSeparator;
 
 //    state.ThirdString.AsSpan().CopyTo(chars.Slice(position));
-//});
 
-//Console.WriteLine(myString);
+//    if (!context.ShowYear) return;
+
+//    position += state.ThirdString.Length;
+//    chars[position++] = spaceSeparator;
+
+//    YearPrefix.CopyTo(chars.Slice(position));
+//    position += YearPrefix.Length;
+//    state.DateTime.Year.TryFormat(chars.Slice(position), out _);
+//});
+//
+// Console.WriteLine(myString);
